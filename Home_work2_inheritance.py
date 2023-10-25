daa95160ac8b6a5b58e2a50a559ea950f7583fa1
dviika <kalinichenko1988@gmail.com>
2023-10-24 22:56:22 -0400

    Applied changes

--- Home_work2_inheritance.py
+++ Home_work2_inheritance.py
@@ -1,18 +1,24 @@
-class Human:
-    def __init__(self, age): #initialize global variable of the class Human
-        self.age = age
-        self.favorite_drink = 'beer'
+#Task create class Human in which define a method drink. During initializqation we accept age
+#Create class vsriable favorite drink = 'beer'
+#Create class Worket inherited from base Human and adding salary
 
-    def drink(self):
+#Tasks
+#method drink -> should print name of the class and add
+class Human:
+    favorite_drink = 'beer'
+
+    def __init__(self, age):  # initialize global variable of the class Human
+        self.age = age
         if self.age < 18:
             self.favorite_drink = 'juice'
-        print("Favorite drink: ", self.favorite_drink)
-        print(f"{type(self).__name__} likes drink {self.favorite_drink}")
+
+    def drink(self):
+        print(f"{self.__class__.__name__} likes drink {self.favorite_drink}")
 
 
 class Worker(Human):
     def __init__(self, age, salary):
-        super().__init__(age) #classWorker doesn't have age we accesing age of Parent class by super().__init___(age)
+        super().__init__(age)  # classWorker doesn't have age we accesing age of Parent class by super().__init___(age)
         self.salary = salary
         if salary > 1000:
             self.favorite_drink = "whiskey"
@@ -23,3 +29,6 @@ Bob.drink()
 
 Josh = Worker(age=35, salary=1200)
 Josh.drink()
+
+print(Human.favorite_drink)
+print(Human(18).favorite_drink)
