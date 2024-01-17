83f4966f97d8279d2330a5f087fdafec70b14e6e
alex <kalinichenko1988@gmail.com>
2024-01-17 13:55:55 -0500

    Delete Home_work_10_rozetka directory

--- Home_work_10_rozetka/locators/locators.py
+++
@@ -1,7 +0,0 @@
-first_pagination_locator = '//li[@class="pagination__item"]/a[contains(@class,"pagination__link pagination__link--active")]'
-previous_btn_at_the_bottom_locator = '//div[@class="pagination ng-star-inserted"]/a[contains(@title,"До попередньої сторінки")]'  # '//div[@class="pagination ng-star-inserted"]/a[contains(@class,"button button--gray button")]'
-next_btn_at_the_bottom_locator = '//div[@class="pagination ng-star-inserted"]/a[contains(@title,"До наступної сторінки")]'  # '//div[@class="pagination ng-star-inserted"]/a[contains(@class,"pagination__direction--forward")]'
-second_paginator_locator = '//div[@class="pagination ng-star-inserted"]//a[contains(text(),"2")]'
-laptop_images_per_default = '//img[contains(@class,"lazy_img_hover")]'
-laptop_list_item = '//ul[@class="catalog-grid ng-star-inserted"]/li[@class="catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted"]//rz-button-product-page[@class ="ng-star-inserted"]//span[@class = "goods-tile__title"]'
-checkbox_apple = '//ul[@class="checkbox-filter"]/li[@class="checkbox-filter__item ng-star-inserted"]/a[@data-id = "Apple"]'
